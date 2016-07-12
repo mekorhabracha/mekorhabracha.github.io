5aee83daa335d084fd4925a37db6e109478a079b
Andrés Catalán <a@catalandr.es>
2016-07-12 14:50:21 -0400

    Fixing JS for archived website

--- old/www.ccshul.com/js/main.js
+++ old/www.ccshul.com/js/main.js
@@ -6,12 +6,12 @@ function swapImg(id, on) {
 		if (p.hasClassName('active')) activeId = id;
 		else {
 			p.addClassName('active');
-			im.src='images/buttons/thumb'+id+'-1.png';
+			im.src='/old/www.ccshul.com/images/buttons/thumb'+id+'-1.png';
 		}
 	} else {
 		if ( id != activeId ) {
 			p.removeClassName('active');
-			im.src='images/buttons/thumb'+id+'-0.png';
+			im.src='/old/www.ccshul.com/images/buttons/thumb'+id+'-0.png';
 		}
 	}
 }
