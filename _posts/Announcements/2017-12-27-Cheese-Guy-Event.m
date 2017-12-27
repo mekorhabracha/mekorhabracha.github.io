530cd68a260d9c0e83a97c2fbfe5df1fd035a5a2
jrafsky <rafskypsychology@aol.com>
2017-12-27 18:46:13 -0500

    Update 2017-12-27-Cheese-Guy-Event.m

--- _posts/Announcements/2017-12-27-Cheese-Guy-Event.m
+++ _posts/Announcements/2017-12-27-Cheese-Guy-Event.m
@@ -1,1 +1,15 @@
+---
+title: Wine and Cheese Tasting with The Cheese Guy
+date: 2017-12-27 00:00 UTC
+section: bulletinboard
+until: 2018-01-20 00:00:00 UTC
+frontpage: true
+layout: post
+media: Announcement
+---
 
+Wine and Cheese Tasting with The Cheese Guy
+Saturday evening, January 20 @ 7:30 PM at Mekor Habracha
+
+Enjoy tasting a wide range of kosher cheese, crackers, jams, spreads, & wines, while learning more about ideal pairings and kosher cheese.   Gourmet cheese will be available for purchase from The Cheese Guy, using cash or credit card; Mekor Habracha will receive 25% of cheese sales.
+Tickets are $25 in advance (deadline is Thursday Jan.18) or $30 (cash only) at the door.  100% of ticket sales will benefit Mekor Habracha.
